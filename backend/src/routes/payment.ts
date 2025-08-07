71f76f6f15a1c5b396ea6c87b981eac698853c3c
Fernand William <fernandwilliam23@gmail.com>
2025-08-07 16:19:30 +0700

    Configed payment.ts routes

---
+++ backend/src/routes/payment.ts
@@ -0,0 +1,18 @@
+import {Router} from 'express'
+import {
+    getPayment,
+    getPaymentById,
+    createPayment,
+    updatePayment,
+    deletePayment
+} from '../controllers/PaymentController'
+
+const router = Router()
+
+router.get('/', getPayment)
+router.get('/:id', getPaymentById)
+router.post('/', createPayment)
+router.put('/:id', updatePayment)
+router.delete('/:id', deletePayment)
+
+export default router
