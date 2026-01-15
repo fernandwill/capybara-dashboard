13790f49285fe9a0521065f8f17994ac37cfcdea
Fernand William <156734117+fernandwill@users.noreply.github.com>
2026-01-15 21:27:57 +0700

    fix: changed supabase client to be server-side and client-side

--- frontend/src/lib/supabaseClient.ts
+++ frontend/src/lib/supabaseClient.ts
@@ -1,8 +1,15 @@
-import { createClient } from '@supabase/supabase-js'
+"use client";
 
-// Get environment variables
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+import {createBrowserClient} from "@supabase/ssr";
 
-// Create Supabase client
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export function createClient() {
+    return createBrowserClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+}
+
+export const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
